1a4f3fe01edb91f87b99138b6fa887229dfad378
brunales21 <bemimarket@gmail.com>
2022-10-14 22:32:32 +0200

    Ahorcado working correctly.

---
+++ JavaLearning-main/src/com/brunales/Main.java
@@ -0,0 +1,10 @@
+package com.brunales;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Juego.ahorcado();
+    }
+}
